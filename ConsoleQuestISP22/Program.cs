4ef1b8265347eb661e28efdb2c94f3efc2bb6da4
AndrewKW <accumhelp@ya.ru>
2024-02-01 10:39:13 +0300

    25-1vvvv

--- ConsoleQuestISP22/Program.cs
+++ ConsoleQuestISP22/Program.cs
@@ -1,9 +1,10 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+
+
+    static int Scene00()
     {
         string userSelect = "";
-
         Console.WriteLine("+++++++++++++++++++++++++++++++");
         Console.WriteLine("+ Вы стоите у ворот техникума +");
         Console.WriteLine("+++++++++++++++++++++++++++++++");
@@ -14,6 +15,71 @@
         userSelect = Console.ReadLine();
         Console.Beep();
         Console.WriteLine(userSelect[0]);
+        switch (userSelect[0])
+        {
+            case '1':
+                Console.WriteLine("Вы выбрали первый вариант.");
+                return 1;
+                break;
+            case '2':
+                Console.WriteLine("Вы выбрали второй вариант.");
+                return 2;
+                break;
+            case '3':
+                Console.WriteLine("Вы выбрали третий вариант.");
+                return 3;
+                break;
+            default:
+                Console.WriteLine("Вы выбрали что-то ещё.");
+                Scene00();
+                return 0;
+                break;
+        }
+    }
+
+    static void Scene0()
+    {
+        string userSelect = "";
+        Console.WriteLine("+++++++++++++++++++++++++++++++");
+        Console.WriteLine("+ Вы стоите у ворот техникума +");
+        Console.WriteLine("+++++++++++++++++++++++++++++++");
+        Console.Write("Что вы будете делать?\n");
+        Console.Write("1. Войду \n2. Уйду \n3.Жрац хочу\n");
+        Console.WriteLine("+++++++++++++++++++++++++++++++");
+        Console.Write("> ");
+        userSelect = Console.ReadLine();
+        Console.Beep();
+        Console.WriteLine(userSelect[0]);
+        switch (userSelect[0])
+        {
+            case '1':
+                Console.WriteLine("Вы выбрали первый вариант.");
+                Scene1();
+                break;
+            case '2':
+                Console.WriteLine("Вы выбрали второй вариант.");
+                break;
+            case '3':
+                Console.WriteLine("Вы выбрали третий вариант.");
+                break;
+            default:
+                Console.WriteLine("Вы выбрали что-то ещё.");
+                Scene0();
+                break;
+        }
+    }
+    static void Scene1()
+    {
+        Console.WriteLine("+++++++++++++++++++++++++++++++");
+        Console.WriteLine("+ Сцена 1 +");
+        Console.WriteLine("+++++++++++++++++++++++++++++++");
+        Console.Write("Что вы будете делать?\n");
+        Console.Write("1. 0 \n2. 1 \n3.2\n");
+        Console.WriteLine("+++++++++++++++++++++++++++++++");
+    }
+
+    private static void Main(string[] args)
+    {
         /*
         if (userSelect[0] == '1') 
             {
@@ -28,21 +94,12 @@
             Console.WriteLine("Вы выбрали третий вариант.");
         }
         */
-        switch (userSelect[0])
-        {
-            case '1':
-                Console.WriteLine("Вы выбрали первый вариант.");
-                break;
-            case '2':
-                Console.WriteLine("Вы выбрали второй вариант.");
-                break;
-            case '3':
-                Console.WriteLine("Вы выбрали третий вариант.");
-                break;
-            default:
-                Console.WriteLine("Вы выбрали что-то ещё.");
-                break;
-        }
+
+        //Scene0();
+
+        Console.WriteLine(Scene00());
+        Console.ReadKey();
+        
 
 
     }   
